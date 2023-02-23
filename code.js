c09ea5fa4ab67890ef202030a98ccdf0042dcda2
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-02-23 11:06:15 -0600

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -4,18 +4,6 @@ const helloWorld = function() {
 }
 
 const sayHello = function(input){
-    if (input){
         return `Hello, ${input}!`;
-    }
-    // else if(input === "Pat"){
-    //     return "Hello, Pat";
-    // }
-   else {return  "Hello, Jane!"}
 }
 
-// const sayHello = function (input){
-//     if (input === "Pat") return "Hello, Pat!"
-//     if (input === "Alex") return "Hello, Alex!"
-//     if (input === "Jane") return "Hello, Jane!"
-//     else return ""
-// }
--- tests.js
+++ tests.js
@@ -22,7 +22,7 @@ describe('sayHello',function (){
         expect(typeof sayHello()).toBe("string");
     });
     it('should return the string "Hello, Jane!" when executed', function(){
-        expect(sayHello()).toBe("Hello, Jane!");
+        expect(sayHello('Jane')).toBe("Hello, Jane!");
     });
     it('should return the string "Hello, Alex!" when executed', function(){
         expect(sayHello('Alex')).toBe("Hello, Alex!");
