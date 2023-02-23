ad527d4f22181f78ce8dfa02b25d3ba4d702bade
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-02-23 10:04:18 -0600

    Add the third test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -21,5 +21,8 @@ describe('sayHello',function (){
     it('should return a string when called', function (){
         expect(typeof sayHello()).toBe("string");
     });
+    it('should return the string "Hello, Jane!" when executed', function(){
+        expect(sayHello()).toBe("Hello, Jane!");
+    });
 
 });
